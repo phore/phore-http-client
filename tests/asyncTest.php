00840225cbf65b5842f63b1e30b0e33c58da74bc
Matthias <m@tth.es>
2019-12-05 13:47:59 +0100

    reschedule

--- tests/asyncTest.php
+++ tests/asyncTest.php
@@ -22,14 +22,24 @@ phore_out("Start");
 
 $err = 0;
 $ok = 0;
-for ($i=0; $i<200; $i++) {
-    $queue->queue(phore_http_request("https://ulan./")->withTimeout(2,10))->then(
-        function(PhoreHttpResponse $response) use (&$data, &$ok)  {
+for ($i=0; $i<10; $i++) {
+    $queue->queue(phore_http_request("http://localhost/")->withTimeout(2,10))->then(
+        function(PhoreHttpResponse $response) use (&$data, &$ok, $queue)  {
+
+            phore_out("queuing new...");
+            $req = phore_http_request("http://localhost/");
+            $queue->queue($req)->then(function (PhoreHttpResponse $e) {
+                echo "OK Sub";
+            });
+            phore_out("queuing new...");
+            $req = phore_http_request("http://localhost/");
+            $queue->queue($req);
             phore_out("OK$ok:");
             $ok++;
 
         },
         function (PhoreHttpRequestException $ex) use (&$err){
+
             phore_out("ERR$err:" . $ex->getMessage());
             $err++;
         });
